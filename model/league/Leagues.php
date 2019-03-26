53f92173655e0c9ed2eef33a56607a46a3634e36
Benjamin HAISE <43814190+HaiseB@users.noreply.github.com>
2019-03-26 21:24:44 +0100

    Ajustement (bug google drive...)

--- model/league/Leagues.php
+++ model/league/Leagues.php
@@ -12,14 +12,13 @@ function deleteLeague() : array{
     $req = $pdo->prepare("SELECT * FROM leagues WHERE id=?");
     $req->execute([$_GET['id']]);
     $league = $req->fetch();
-    
     if (empty($league)){
         $_SESSION['flash']['danger'] = "La ligue que vous cherchez à supprimer n'existe pas";
         header('location: index.php?action=league');
         exit();
     } else {
         $pdo->prepare('DELETE FROM leagues WHERE id = ?')->execute([$_GET['id']]);
-        $_SESSION['flash']['danger'] = "La ligue à bien été supprimmée";
+        $_SESSION['flash']['success'] = "La ligue à bien été supprimée";
         header('location: index.php?action=league');
         exit();
     }
